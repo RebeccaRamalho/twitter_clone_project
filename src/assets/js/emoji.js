f69125b3eba0b3328c0e6d36589dd6f7c6332c94
RebeccaKanu <kanurebecca0@gmail.com>
2021-07-20 17:05:49 +0200

    <feat(emoji.js) adding of the random moji features in progress>                                                                                                                                                                                                         <random emo when the mouse is over the default emo that appear when the page is charged>

--- src/assets/js/emoji.js
+++ src/assets/js/emoji.js
@@ -1,29 +1,30 @@
 /*I-Emojis appear*/
 
-//Retreival of the element where the event click occur:
+//Request to the DOM of the element where the event click occur:
 const htmlEmo = Array.from(document.getElementById("emoji").children);
 //=>[<div>, <div>, ... ]
 
 htmlEmo.forEach((element, index) => {
   let emoInDiv = htmlEmo[index]; //looping through the htmlEmo array. Result=> <div>"😋"</div>
-
   let emoInString = htmlEmo[index].textContent; //Result => "😋"
 
   emoInDiv.addEventListener("click", function () {
-    //the user must click on an html element
-    let boxMessage = document.getElementById("message").value; //<e retreive the textArea value where we want to add the emoji
-
-    const tweets = boxMessage + emoInString; //adding the emoji selected to the textArea value
-
-    document.getElementById("message").value = tweets; //we inject the new value to the textArea
+    //request to the DOM of the textArea value where we want to add the emoji
+    let boxMessage = document.getElementById("message").value;
+    //adding the emoji selected to the textArea value
+    const tweets = boxMessage + emoInString;
+    //we inject the new value to the textArea
+    document.getElementById("message").value = tweets;
   });
 });
 
 /*II_Emoji change opacity*/
 
-const originalEmo = document.getElementById("appearEmo"); //html el where we gonna click to make appear all the emoji
+//html el where we gonna click to make appear all the emoji
+const originalEmo = document.getElementById("appearEmo");
+//
 const EmojiId = document.getElementById("emoji");
-
+//
 originalEmo.addEventListener("click", function () {
   EmojiId.style.opacity = 1;
 });
@@ -32,28 +33,40 @@ originalEmo.addEventListener("click", function () {
 
 getRandomInt = (max) => {
   return Math.floor(Math.random() * max);
-}; //getting radom int between 1 an 2
-
-/******/
-
-let emojiIndex = getRandomInt(3); //i retreive a number between 0 and 2
-const randomEmoji = document.querySelector("#appearEmo").children; //i retreive all the emoji
+}; //getting radom int between 1 and 2
 
 /***/
+//i retreive a number between 0 and 2
+let emojiIndex = getRandomInt(3);
+//i retreive all the emoji
+const randomEmoji = document.querySelector("#appearEmo").children;
 
-let htmlRandomeEmo = document.querySelector("#randomEmoAppear"); //html el where emo is pushed
+console.log("eee", randomEmoji);
+
+/***/
+//request to the DOM of the html element where emo is pushed
+let htmlRandomeEmo = document.querySelector("#randomEmoAppear");
 // let gg = document.querySelector("#randomEmoAppearsss"); //element where the is hover event
-let em = randomEmoji[emojiIndex].textContent; //random emo between 1 an 2 index
-let re = randomEmoji[emojiIndex]; //random emo where my mouse is on
-console.log("random emo", htmlRandomeEmo);
+//
+//random emo between 1 an 2 index
+let em = randomEmoji[emojiIndex].textContent;
+let re = randomEmoji[emojiIndex];
+let emTest = document.querySelector("#randomEm");
+//random emo where my mouse is on
+console.log("random emo", em);
 
 /***/
-function myScript(htmlRandomeEmo) {
-  htmlRandomeEmo.innerHTML = randomEmoji[emojiIndex].textContent; //i insert a random number in the emoji index
-}
+// function myScript(htmlRandomeEmo) {
+//   //i insert a random number in the emoji index
+//   htmlRandomeEmo.innerHTML = randomEmoji[emojiIndex].textContent;
+// }
 /***/
-htmlRandomeEmo.addEventListener("mouseover", function () {
-  myScript(htmlRandomeEmo);
-  console.log("hummm", htmlRandomeEmo);
+emTest.addEventListener("mouseover", function () {
+  // myScript(htmlRandomeEmo);
+  emTest.textContent = em;
+  ///
+  // reset the color after a short delay
+
+  ///
 });
 /***/
